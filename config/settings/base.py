3fae7454df337eb41db3ffc9259fc21084d266a2
nishantKuvaka <nishant@kuvaka.io>
2026-07-08 15:34:49 +0530

    fix: Standardize string quotes for DJANGO_SETTINGS_MODULE in configuration files

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,9 +76,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # ------------------------------------------------------------------------------
 AUTH_USER_MODEL = "users.User"
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
